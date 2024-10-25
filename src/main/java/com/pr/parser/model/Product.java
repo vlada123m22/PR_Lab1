153225390d22cf8ad8ac7c05def5b042c410c08b
Vlada <vlada.musin@gmail.com>
2024-10-25 13:37:51 +0300

    Extract name and price

---
+++ src/main/java/com/pr/parser/model/Product.java
@@ -0,0 +1,8 @@
+package com.pr.parser.model;
+
+import lombok.Data;
+@Data
+public class Product {
+    private String name;
+    private String price;
+}
--- src/main/java/com/pr/parser/service/ScrappingService.java
+++ src/main/java/com/pr/parser/service/ScrappingService.java
@@ -1,20 +1,44 @@
 package com.pr.parser.service;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import com.pr.parser.model.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ScrappingService {
     private final WebClientService webClientService;
 
+    public List<Product> parseHtmlForProducts(String html) {
+        List<Product> products = new ArrayList<>();
+        Document document = Jsoup.parse(html);
+        Elements productElements = document.select(".js-itemsList-item");
+        for (Element productElement : productElements) {
+            var productName = productElement.select("meta[itemprop=name]").attr("content");
+            var productPrice = productElement.select(".card-price_curr").text();
+            var product = new Product();
+            product.setName(productName);
+            product.setPrice(productPrice);
+            products.add(product);
+        }
+        return products;
+    }
+
 
     public String scrapPage() {
         Mono<String> result = webClientService.fetchHtmlContent("/catalog/electronics/telephones/mobile/?page_=page_3");
         result.subscribe(htmlContent -> {
-            System.out.println("HTML Content from relative path: ");
-            System.out.println(htmlContent);
+            List<Product> products = parseHtmlForProducts(htmlContent);
+            products.forEach(System.out::println);
+            System.out.println("Total products: " + products.size());
         });
         return result.block();
     }
